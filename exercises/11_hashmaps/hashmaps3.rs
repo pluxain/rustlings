2768f5f20e11cf40df7063896578e3720df92938
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-05 15:01:34 +0100

    ex11: fix hashmaps3

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,26 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let team_1_team_score = scores
+            .entry(team_1_name)
+            .or_insert_with(TeamScores::default);
+        // team_1_team_score = TeamScores {
+        //     goals_scored: team_1_team_score.goals_scored + team_1_score,
+        //     goals_conceded: team_1_team_score.goals_conceded + team_2_score,
+        // };
+
+        team_1_team_score.goals_scored += team_1_score;
+        team_1_team_score.goals_conceded += team_2_score;
+
+        let team_2_team_score = scores
+            .entry(team_2_name)
+            .or_insert_with(TeamScores::default);
+        *team_2_team_score = TeamScores {
+            goals_scored: team_2_team_score.goals_scored + team_2_score,
+            goals_conceded: team_2_team_score.goals_conceded + team_1_score,
+        };
+        // team_2_team_score.goals_scored += team_2_score;
+        // team_2_team_score.goals_conceded += team_1_score;
     }
 
     scores
